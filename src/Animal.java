412def452d8cbbc338703223e37389587d8fcdc5
Vyvy129H <147341690+Vyvy129H@users.noreply.github.com>
2024-07-29 11:41:57 +0700

    Update

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Animal {
+//Lớp trừu tượng Animal
+public abstract class Animal {
+//    Phương thức trừu tượng eat
+    public abstract void eat();
+    public void makeSound() {
+        System.out.println("Animal makeSound");
+    }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Cat {
+//Lớp cat kế thừa lớp Animal
+public class Cat extends  Animal{
+//    ví dụ về override
+    @Override
+    public void eat() {
+        System.out.println("cat eat");
+    }
+
+    //    ví dụ về overloading
+    public void makeSound(String sound) {
+        System.out.println("Cat makeSound " + sound);
+    }
+
 }
